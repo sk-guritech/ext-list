7d8baa4f785c2f3efd76773a09f1ed3a53841dbf
@GuriTech/ Sakaguchi Ryo <sakaguchi@sk-techfirm.com>
2023-05-15 03:04:05 +0000

    relaxing type restrictions

--- tests/__iadd__/iadd_error_test.py
+++ tests/__iadd__/iadd_error_test.py
@@ -5,17 +5,6 @@ import pytest
 from ext_list import ExtList
 
 
-def test_raise_type_error_by_operating_between_different_types():
-    ext_list_1 = ExtList([1, 2, 3])
-    ext_list_2 = ExtList(['4', '5', '6'])
-
-    with pytest.raises(TypeError):
-        ext_list_1 += ext_list_2
-
-    with pytest.raises(TypeError):
-        ext_list_2 += ext_list_1
-
-
 def test_raise_type_error_by_assign_non_ExtList_object():
     ext_list_1 = ExtList([1, 2, 3])
     normal_list_1 = [4, 5, 6]
